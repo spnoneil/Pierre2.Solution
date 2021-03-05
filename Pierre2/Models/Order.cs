67bacd678fd84a79d7b6d34388bb924b41362542
Scott O'Neil <scottieoneil@gmail.com>
2021-03-05 12:16:06 -0800

    add order find method

--- Pierre2/Models/Order.cs
+++ Pierre2/Models/Order.cs
@@ -20,9 +20,18 @@ namespace Bakery.Models
       Id = _instances.Count;
     }
 
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 
