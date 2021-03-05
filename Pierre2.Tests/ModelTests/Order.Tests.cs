b0b03c125669663866129c1df500e8100194b86b
Scott O'Neil <scottieoneil@gmail.com>
2021-03-05 09:09:25 -0800

    test order constructor

---
+++ Pierre2.Tests/ModelTests/Order.Tests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreateInstanceOfOrder_Order()
+    {
+      Order testOrder = new Order("test title", "test desc", 8, "test date");
+      Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+  }
+}
---
+++ Pierre2/Models/Order.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Order 
+  {
+    public string OrderTitle { get; set; }
+    public string OrderDescription { get; set; }
+    public int OrderPrice { get; set; }
+    public string OrderDate { get; set; }
+    public int Id { get; private set; }
+    private static List<Order> _instances = new List<Order> {};
+    public Order(string title, string desc, int price, string date)
+    {
+      OrderTitle = title;
+      OrderDescription = desc;
+      OrderPrice = price;
+      OrderDate = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+
+
+}
