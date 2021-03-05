0e7aceba7d6c25a97f60b94a59a4d4acb0ae80fa
Scott O'Neil <scottieoneil@gmail.com>
2021-03-05 10:19:22 -0800

    link splash / vendor page

---
+++ Pierre2/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
