315c0c739d76063299c17e46992f35f0aad2f675
Scott O'Neil <scottieoneil@gmail.com>
2021-03-05 14:28:35 -0800

    update vendor controller/views

--- Pierre2/Controllers/VendorsController.cs
+++ Pierre2/Controllers/VendorsController.cs
@@ -34,5 +34,17 @@ namespace Bakery.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>(); 
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
