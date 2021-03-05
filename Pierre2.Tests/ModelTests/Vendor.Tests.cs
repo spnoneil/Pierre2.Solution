2c67a8f21150850178652bf490fe11fe1a119fe3
Scott O'Neil <scottieoneil@gmail.com>
2021-03-05 08:38:47 -0800

    pass name/desc test

--- Pierre2.Tests/ModelTests/Vendor.Tests.cs
+++ Pierre2.Tests/ModelTests/Vendor.Tests.cs
@@ -14,5 +14,16 @@ namespace Bakery.Tests
       Vendor testVendor = new Vendor("test vendor", "this vendor is a test");
       Assert.AreEqual(typeof(Vendor), testVendor.GetType());
     }
+    [TestMethod]
+    public void GetNameAndDescription_ReturnsVendorNameAndDescription_String()
+    {
+      string name = "test vendor";
+      string description = "this vendor is a test";
+      Vendor testVendor = new Vendor(name, description);
+      string nameResult = testVendor.VendorName;
+      string descriptionResult = testVendor.VendorDescription;
+      Assert.AreEqual(name, nameResult);
+      Assert.AreEqual(description, descriptionResult);
+    }
   }
 }
