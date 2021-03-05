16f572a3748b34653deac78f82b440822e344d49
Scott O'Neil <scottieoneil@gmail.com>
2021-03-05 09:14:40 -0800

    test order properties

--- Pierre2.Tests/ModelTests/Order.Tests.cs
+++ Pierre2.Tests/ModelTests/Order.Tests.cs
@@ -19,5 +19,22 @@ namespace Bakery.Tests
       Order testOrder = new Order("test title", "test desc", 8, "test date");
       Assert.AreEqual(typeof(Order), testOrder.GetType());
     }
+    [TestMethod]
+    public void GetProperties_ReturnsProperties_StringOrInt()
+    {
+      string title = "test title";
+      string desc = "test desc";
+      int price = 8;
+      string date = "test date";
+      Order testOrder = new Order(title, desc, price, date);
+      string resultTitle = testOrder.OrderTitle;
+      string resultDesc = testOrder.OrderDescription;
+      int resultPrice = testOrder.OrderPrice;
+      string resultDate = testOrder.OrderDate;
+      Assert.AreEqual(title, resultTitle);
+      Assert.AreEqual(desc, resultDesc);
+      Assert.AreEqual(price, resultPrice);
+      Assert.AreEqual(date, resultDate);
+    }
   }
 }
